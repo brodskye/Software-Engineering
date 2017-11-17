3df42c6e04c1c0598969b863db9db3d9b3e9a279
asunciod <asunciod@oregonstate.edu>
2017-11-17 15:04:38 -0800

    updating to add manual and partitioning tests

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -35,17 +35,98 @@ public class UrlValidatorTest extends TestCase {
    public UrlValidatorTest(String testName) {
       super(testName);
    }
-
    
-   
-   public void testManualTest()
+   public static void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   //System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   
+	   System.out.println("------------------------- MANUALLY TESTING URLS -----------------------\n");
+	   
+	   System.out.println("TEST 1: PROTOCOLS");
+	   
+	   //manual test protocol
+	   System.out.println("URL: \"https://www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid("https://www.amazon.com"));	//expected = pass, actual = pass
+	   System.out.println("\nURL: \"ftp://www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid("ftp://www.amazon.com"));		//expected = pass, actual = pass
+	   System.out.println("\nURL: \":www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid(":www.amazon.com"));			//expected = fail, actual = pass -- bug
+	   System.out.println("\nURL: \"100e://www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid("100e://www.amazon.com"));	//expected = fail, actual = fail
+	   System.out.println("\nURL: \"asdf://www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid("asdf://www.amazon.com"));	//expected = pass, actual = pass
+	   System.out.println("\nURL: \"www.amazon.com\"");
+	   System.out.println("Expected: True, Actual: " + urlVal.isValid("www.amazon.com"));			//expected = fail, actual = fail
 	   
 	   
    }
    
+   // main test function to test all input partitions
+   public static void testAllPartitions() {
+	   
+	   System.out.println("\n------------------------- INPUT PARTITIONING TESTS -----------------------\n");
+	   
+	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	   
+	   //input partition table scheme
+	   boolean[][] table_scheme = {
+	        {true,true,true,true},
+	        {false,false,false,false},
+	        {false,true,true,true},
+	        {true,false,true,true},
+	        {true,true,false,true},
+	        {true,true,true,false},
+	        {false,false,true,true},
+	        {false,true,false,true},
+	        {true,false,true,false},
+	        {true,true,false,false},
+	        {false,true,true,false},
+	        {true,false,false,true},
+	        {false,false,false,true},
+	        {true,false,false,false},
+	        {false,false,true,false},
+	        {false,true,false,false}
+	    };
+
+	    //loop through boolean 2D array
+	    for(int i = 0; i < table_scheme.length; i++){
+	        	
+			//loop through partitions
+	        for(int j = 0; j < 5; j++){
+	            String test_url = testProtocolPartition(table_scheme[i][0], j) + testProtocolPartition(table_scheme[i][1], j) 
+	                + testProtocolPartition(table_scheme[i][2], j) + testProtocolPartition(table_scheme[i][3], j);
+	            
+	            //get expected boolean for this URL
+	            boolean check_expected = table_scheme[i][0] && table_scheme[i][1] && table_scheme[i][2] && table_scheme[i][3];
+	            
+	            //print out URL and expected/actual results
+	            System.out.println(test_url);
+	            System.out.println("Expected: " + check_expected + ", Actual: " + urlVal.isValid(test_url) + "\n");
+	        }
+
+	    }
+   }
+   
+	 //input: 
+	   //boolean: returns a string from either a valid string array or invalid string array
+	   //integer: index value of array
+	//precondition: all partition array sizes must be the same length
+	public static String testProtocolPartition(boolean isValid, int index){
+		
+	   //array for valid protocols
+	   String[] valid = {"http://", "https://", "ftp://", "cid://", "mailto:", ""};
+	   
+	   //array for invalid protocols
+	   String[] invalid = {"345://", "http:/", "https:", "https/", ":::", "-&@"};
+	
+	   if(isValid){
+	       return valid[index];
+	   }else{
+	       return invalid[index];
+	   }
+	
+	}
    
    public void testYourFirstPartition()
    {
